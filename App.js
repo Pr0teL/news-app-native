c6e325d92f0322d8f305f84231f119a31de83c31
Victor <user@MacBook-Air-user.local>
2022-12-28 00:49:08 +0300

    Add Post Component

--- App.js
+++ App.js
@@ -1,6 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import {Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 import styled from 'styled-components/native';
+import Post from './components/Post'
+
+const MainPage = styled.View`
+   padding-top: 13%;
+   padding-left: 10px;
+   padding-right: 10px;
+   height: 100%;
+   background-color: #f8f8f8;
+`;
+
+const Header = styled.View`
+   width: 100%;
+   margin: 0 auto;
+   margin-bottom: 10px;
+   align-items: center;
+   flex-direction: row;
+   justify-content: space-between
+`;
 
 const Title = styled.Text`
    font-size: 30px;
@@ -8,54 +26,26 @@ const Title = styled.Text`
    margin-bottom: 3%;
 `;
 
-const MainPage = styled.View`
-   padding-top: 10%;
-   padding-left: 10px;
-   padding-right: 10px;
-   height: 100%;
-   background-color: #f8f8f8;
-`;
-
-const Post = styled.View`
-   
-   background-color: #fff;
-   height: 350px;
-   width: 100%;
-   border-radius: 16px;
-   margin: 0 auto;
-   padding: 5px;
-   box-shadow: 0px 14px 30px rgba(0, 0, 0, 0.05);
-`;
-
-const PostCategory = styled.Text`
-   background-color: ##3380EF;
-   height: 20px;
-   width: 30%;
-   border-radius: 16px;
-`;
-
-const PostImage = styled.Image`
-   background-color: gray;
-   height: 200px;
-   width: 100%;
-   border: 1px solid rgba(0,0,0,0.1);
-   border-radius: 16px;
-   margin: 0 auto;
-   margin-bottom: 10px;
+const Search = styled.TextInput`
+   margin-bottom: 3%;
+   border: 2px solid rgba(0,0,0,0.1);
+   font-size: 22px;
+   padding: 5px 10px;
+   width: 200px;
+   border-radius: 10px;
 `;
 
 export default function App() {
-  return (
-    <MainPage>
-      <Title>Hot News</Title>
-      <Post>
-        <PostImage source={{ uri: "https://newsinitiative.withgoogle.com/info/assets/static/images/news-youtube/hero-image-a0d28e8991ec7398d914388a83d78d56.jpg"}}>
-        </PostImage>
-        <Title style={{fontSize:"25px", marginLeft: "3%"}}>8 Steps To Help You Stop Overthinking Everything</Title>
-      </Post>
-      <StatusBar style="auto" />
-    </MainPage>
-  );
+   return (
+      <MainPage>
+         <Header>
+            <Title>IT NEWS</Title>
+            <Search placeholder={"Поиск..."}/>
+         </Header>
+         <Post createdAt={"10.01.2023"} author={"Victor Botov"} title={"Более 30% IT-специалистов уехали из России или планируют релокацию"} imageUrl={"https://rsv.ru/blog/wp-content/uploads/2022/04/rear-view-of-programmer-working-all-night-long-918x516.jpg"}/>
+         <StatusBar style="auto" />
+      </MainPage>
+   );
 }
 
 
---
+++ components/Post.js
@@ -0,0 +1,51 @@
+import { Text, View, TextInput } from 'react-native';
+import styled from 'styled-components/native';
+
+const Title = styled.Text`
+   font-size: 30px;
+   font-weight: 500;
+   margin-bottom: 2%;
+`;
+
+
+const PostView = styled.View`
+   
+   background-color: #fff;
+   height: 350px;
+   width: 100%;
+   border-radius: 16px;
+   margin: 0 auto;
+   padding: 5px;
+   box-shadow: 0px 14px 30px rgba(0, 0, 0, 0.05);
+`;
+
+
+
+const PostImage = styled.Image`
+   background-color: gray;
+   height: 200px;
+   width: 100%;
+   border: 1px solid rgba(0,0,0,0.1);
+   border-radius: 16px;
+   margin: 0 auto;
+   margin-bottom: 10px;
+`;
+
+
+const Post = ({title, imageUrl, createdAt, author, top = true}) => {
+    return (<PostView>
+        <PostImage source={{ uri: imageUrl }}>
+        </PostImage>
+        <View style={{ flexDirection: "row", justifyContent: "space-between", paddingLeft: "2%", paddingRight: "2%", marginBottom: "2%" }}>
+            <Text style={{ color: "rgba(0,0,0,0.3)" }}>{top ? "🔥 Top news" : "Daily"}</Text>
+            <Text style={{ color: "rgba(0,0,0,0.3)" }}>{createdAt}</Text>
+        </View>
+        <Title style={{ fontSize: "18px", marginLeft: "3%" }}>{title}</Title>
+        <View style={{ flexDirection: "row", justifyContent: "space-between", paddingLeft: "2%", paddingRight: "2%", alignItems: "center" }}>
+            <Text style={{ fontSize: "13px", fontWeight: "400" }}>{author}</Text>
+            <Text style={{ color: "rgba(0,0,0,0.3)", fontSize: "33px", fontWeight: "600", marginBottom: "4%" }}>...</Text>
+        </View>
+    </PostView>)
+}
+
+export default Post
